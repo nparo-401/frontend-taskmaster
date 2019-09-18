911c0bf8e5a579bd004d5050eb01dc0605dbf385
Nicholas Paro <paro.nicholas@gmail.com>
2019-09-18 11:35:39 -0700

    initial commit

---
+++ lambda-dynamo/src/main/java/lambda/dynamo/History.java
@@ -0,0 +1,42 @@
+package lambda.dynamo;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
+
+@DynamoDBDocument
+public class History {
+  private String date;
+  private String action;
+  private String assignee;
+
+  public History(){}
+
+  public History(String date, String action) {
+    this.date = date;
+    this.action = action;
+    this.assignee = "none";
+  }
+
+  public String getDate () {
+    return date;
+  }
+
+  public void setDate (String date) {
+    this.date = date;
+  }
+
+  public String getAction () {
+    return action;
+  }
+
+  public void setAction (String action) {
+    this.action = action;
+  }
+
+  public String getAssignee () {
+    return assignee;
+  }
+
+  public void setAssignee (String assignee) {
+    this.assignee = assignee;
+  }
+}
